28bacbc559196b8a01da1671530bda8c473684c7
AIvashov <ivashov.aleksey@proton.me>
2025-11-21 13:21:33 +0100

    Updated s3 links

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,9 @@ set -e
 
 RELAY_EXEC="${RELAY_EXEC:-/workspace/relay}"
 
-RELAY_VERSION=$(curl -s "https://fortytwo-network-public.s3.us-east-2.amazonaws.com/relay/latest")
+RELAY_VERSION=$(curl -s "https://fortytwo-network-packages-public.s3.us-east-2.amazonaws.com/relay/latest")
 
-DOWNLOAD_RELAY_URL="https://fortytwo-network-public.s3.us-east-2.amazonaws.com/relay/v$RELAY_VERSION/FortytwoProtocolRelay-linux-amd64"
+DOWNLOAD_RELAY_URL="https://fortytwo-network-packages-public.s3.us-east-2.amazonaws.com/relay/v$RELAY_VERSION/FortytwoProtocolRelay-linux-amd64"
 
 curl -fsSL -o "$RELAY_EXEC" "$DOWNLOAD_RELAY_URL"
 
